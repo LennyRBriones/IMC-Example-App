70bef370dab91afb63678bc6b23588a7bd8fe703
LennyRBriones <lennyrbriones@outlook.com>
2023-12-22 20:53:27 -0600

    Add float button, logic of age & weight

--- app/src/main/java/com/example/imcapplication/MainActivity.kt
+++ app/src/main/java/com/example/imcapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.slider.RangeSlider
 import java.text.DecimalFormat
 
@@ -12,11 +13,19 @@ class MainActivity : AppCompatActivity() {
 
     private var isMaleSelected:Boolean = true
     private var isFemaleSelected:Boolean = false
+    private var currentweight: Int = 60
+    private var currentage: Int = 25
 
     private lateinit var viewMale:CardView
     private lateinit var viewFemale:CardView
     private lateinit var txvheight:TextView
     private lateinit var rsheight:RangeSlider
+    private lateinit var btnsubstractweight:FloatingActionButton
+    private lateinit var btnplusweight:FloatingActionButton
+    private lateinit var tvweight:TextView
+    private lateinit var btnsubstractage:FloatingActionButton
+    private lateinit var btnplusage:FloatingActionButton
+    private lateinit var tvage:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +40,12 @@ class MainActivity : AppCompatActivity() {
         viewMale = findViewById(R.id.viewmale)
         txvheight = findViewById(R.id.txvheight)
         rsheight = findViewById(R.id.rsheight)
+        btnplusweight = findViewById(R.id.btnplusweight)
+        btnsubstractweight = findViewById(R.id.btnsubstractweight)
+        tvweight = findViewById(R.id.tvweight)
+        btnplusage = findViewById(R.id.btnplusage)
+        btnsubstractage = findViewById(R.id.btnsubstractage)
+        tvage = findViewById(R.id.tvage)
     }
 
     private fun initListeners(){
@@ -47,6 +62,32 @@ class MainActivity : AppCompatActivity() {
             val result =df.format(value)
             txvheight.text = "$result cm"
         }
+        btnplusweight.setOnClickListener{
+            currentweight += 1
+            setweight()
+        }
+        btnsubstractweight.setOnClickListener{
+            currentweight -= 1
+            setweight()
+        }
+
+        btnplusage.setOnClickListener{
+            currentage += 1
+            setage()
+        }
+
+        btnsubstractage.setOnClickListener{
+            currentage -= 1
+            setage()
+        }
+    }
+
+    private fun setweight(){
+        tvweight.text = currentweight.toString()
+    }
+
+    private fun setage(){
+        tvage.text = currentage.toString()
     }
 
     private fun changeGender(){
@@ -72,5 +113,7 @@ class MainActivity : AppCompatActivity() {
     }
     private fun initUI(){
         setGenderColor()
+        setweight()
+        setage()
     }
 }
